f0028b5482c62d92182cad6467ee73acc9df4655
Abner Oliveira <abnersanto2014@gmail.com>
2022-03-19 10:06:03 -0300

    aula xamarin

---
+++ PAM/Aula_14_03_22/Aula_14_03_22/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Aula_14_03_22
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
